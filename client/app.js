6c61681175ba57404e5e16c9735829f943d2dc94
awinste <awinste@gmail.com>
2017-12-26 12:58:00 -0600

    call loginStatus method to check if user is logged in to facebook already

--- client/app.js
+++ client/app.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        loginStatus: () =>{
+            console.log('login status called!');
+        }
     }
 })
+
